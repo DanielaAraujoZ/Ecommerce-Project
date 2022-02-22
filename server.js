be9e21ee460526486bc1b6bba272ad546adf845a
Daniela Araujo <daraujoz@unal.edu.co>
2022-02-22 11:51:03 -0500

    aditional changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('dataUsers.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("dataUsers.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-const port = process.env.PORT || 5000
-server.listen(port, () => { 
-    console.log('Server is running');
- })
+server.use(middlewares);
+server.use(router);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log("Server is running");
+});
